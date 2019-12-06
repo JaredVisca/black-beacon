9b997f998cab1f58437e8989490cb8d03b4d7464
JaredVisca <jared.visca@joann.com>
2019-12-06 15:15:40 -0500

    Adds a timeout to the api calls
    In the case that the API call reaches out to a server that doesn't
    return a response, this will time out the call instead of leaving it
    open indefinitely.

--- blackbeaconping.py
+++ blackbeaconping.py
@@ -1,10 +1,10 @@
-import os, urllib2, mimetypes, json, datetime
+import os, urllib2, mimetypes, json, datetime, ssl
 
 def black_beacon_ping_url(urls):
     for url in urls:
         try:
             start = datetime.datetime.now()
-            res = urllib2.urlopen(url.get_url())
+            res = urllib2.urlopen(url.get_url(), timeout=17)
             end = datetime.datetime.now()
             delta = (end - start)
             delta = int(((delta.microseconds + 0.0 + (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6) * 1000)
@@ -14,6 +14,8 @@ def black_beacon_ping_url(urls):
         except urllib2.HTTPError, e:
             url.set_http_status(e.code)
             url.set_message('%s\'s url, %s, had a response time of %sms and returned a %s.' % (url.get_title(), url.get_url(), url.get_response_time(), url.get_http_status()))
+		except ssl.SSLError, e:
+			url.set_message('%s\'s url, %s, timed out (timeout is set to 17 seconds).' % (url.get_title(), url.get_url()))
         except urllib2.URLError, e:
             url.set_message('%s\'s url, %s, had a response time of %sms and returned a %s.  This webservice also returned an error: %s' % (url.get_title(), url.get_url(), url.get_response_time(), url.get_http_status(), e.reason))
     return urls
